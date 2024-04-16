c25fc5aede68734ab3012f6bad31935a6a14a7e5
alifbintang1 <alifbintange@gmail.com>
2024-04-16 22:43:51 +0700

    initial commit

---
+++ src/test/java/id/ac/ui/cs/advprog/subscriptionmanagement/SubscriptionManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.subscriptionmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubscriptionManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
