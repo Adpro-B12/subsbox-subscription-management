8965fcd6206fdb9e946ebdce541b58323bad245b
jeanxwang <fazaqinthara629@gmail.com>
2024-04-22 21:57:32 +0700

    Subscription model init

---
+++ src/main/java/id/ac/ui/cs/advprog/subscriptionmanagement/model/Subscription.java
@@ -0,0 +1,21 @@
+package id.ac.ui.cs.advprog.subscriptionmanagement.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter @Setter
+public class Subscription {
+    private Long id;
+    private String uniqueCode;
+    private String status;
+    private Date startDate;
+    private Date endDate;
+    private User user;
+    private SubscriptionBox subscriptionBox;
+
+    public Subscription(String status, Date startDate, Date endDate, User user, SubscriptionBox subscriptionBox) {
+        
+    }
+}
