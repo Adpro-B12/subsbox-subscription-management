5aacc5a93e012da70896df42431a8966dcd82f00
jeanxwang <fazaqinthara629@gmail.com>
2024-05-24 14:42:30 +0700

    Implement monitoring

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,6 +35,8 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    runtimeOnly("io.micrometer:micrometer-registry-prometheus")
     implementation("javax.persistence:javax.persistence-api:2.2")
     compileOnly("org.projectlombok:lombok")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
