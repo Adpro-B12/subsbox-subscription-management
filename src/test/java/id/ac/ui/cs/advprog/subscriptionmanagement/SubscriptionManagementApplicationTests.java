fd5d819350f0a52087c1998090e327bc4a02b86f
alifbintang1 <alifbintange@gmail.com>
2024-05-24 13:33:43 +0700

    add active profiles

--- src/test/java/id/ac/ui/cs/advprog/subscriptionmanagement/SubscriptionManagementApplicationTests.java
+++ src/test/java/id/ac/ui/cs/advprog/subscriptionmanagement/SubscriptionManagementApplicationTests.java
@@ -2,7 +2,8 @@ package id.ac.ui.cs.advprog.subscriptionmanagement;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+import org.springframework.test.context.ActiveProfiles;
+@ActiveProfiles("test")
 @SpringBootTest
 class SubscriptionManagementApplicationTests {
 
